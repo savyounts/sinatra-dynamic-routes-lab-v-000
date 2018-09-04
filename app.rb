36eae71f6ac163c2d65c6e6e6a6a7156f52599e6
Sav Younts <savyounts@gmail.com>
2018-09-04 17:14:27 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @string = params.values.join
+    @string = params.values.join(' ')
     "#{@string}"
   end
 
